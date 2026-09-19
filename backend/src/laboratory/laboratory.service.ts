b6c476c19fe7c76ef83c15185cd8cb43dd57f0d8
Angie Tatiana Alba Amado <angietatianaalbaamado@gmail.com>
2026-09-19 15:23:28 -0500

    feat(laboratory-imaging): conectar modulo en app.module y quitar esqueleto laboratory

--- backend/src/laboratory/laboratory.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class LaboratoryService {
-  findAll() {
-    return { message: 'Módulo Laboratorio e Imágenes - en construcción' };
-  }
-}
